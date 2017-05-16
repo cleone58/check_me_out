453a34b7154af4bf916f747a22141c7496839e12
Cleone Jacob <therealcleonejacob@gmail.com>
2017-05-16 17:34:00 -0400

    fixed typo in “About Me” section

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ class App extends Component {
                              As a Front End Developer with a deep interest of JavaScript and the React Eco-System my aim is to continue to increase my scripting and overall programming knowledge so that I can continue to create really cool apps.</p> <br/>
                           <p>"Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. And the only way to do great work is to love what you do. If you haven't found it yet, keep looking.<br/>
                              Don't settle. As with all matters of the heart, you'll know when you find it."<br/>
-                             Steve Jobbs</p>
+                             Steve Jobs</p>
 
                           </div>
                         </div>
